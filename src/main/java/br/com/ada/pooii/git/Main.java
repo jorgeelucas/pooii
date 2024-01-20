739e45e5604c1b9409ac6bfe597180e767050218
Jorge Lucas <jorge.lucas.14@gmail.com>
2024-01-19 22:19:20 -0300

    git

---
+++ src/main/java/br/com/ada/pooii/git/Main.java
@@ -0,0 +1,8 @@
+package br.com.ada.pooii.git;
+
+public class Main {
+    // git status -> ver mudancas
+    // git add . -> adicionar todas as mudancas ("git add <arquivo>" para adicionar so o arquivo)
+    // git commit -m "mensagem"
+    // git push origin <branch> (develop, main)
+}
