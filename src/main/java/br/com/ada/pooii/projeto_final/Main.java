5d3986037b351c2c4a6210035d3172841481c6df
Jorge Lucas <jorge.lucas.14@gmail.com>
2024-01-24 22:36:01 -0300

    steps

---
+++ src/main/java/br/com/ada/pooii/projeto_final/Main.java
@@ -0,0 +1,11 @@
+package br.com.ada.pooii.projeto_final;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // criar repositorio
+        // criar service
+        // criar controller
+
+    }
+}
